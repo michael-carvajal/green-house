28be6d9f82af1971592885ea3e7ae86fd5da329d
KatiePee <piele.katie@gmail.com>
2023-04-27 13:48:52 -0700

    phase 2

--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,15 @@
+import { useContext } from 'react';
 import './LightSwitch.css';
+import { ThemeContext } from '../../context/ThemeContext';
 
 function LightSwitch() {
+  const {themeName, setThemeName} = useContext(ThemeContext)
+
+  console.log('light switch render--themeName: ', themeName)
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" onClick={() => {setThemeName(prevTheme => 'day')}}>DAY</div>
+      <div className="off" onClick={ () => { setThemeName(prevTheme => 'night')}}>NIGHT</div>
     </div>
   );
 }
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -5,7 +5,7 @@ export const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 
 export default function ThemeProvider({ children }) {
-  const [themeName, setThemeName] = useState("day");
+  const [themeName, setThemeName] = useState("night");
 
   console.log('them context--children from theme provider', children);
 
